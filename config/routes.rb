86252a92ff7c4f43f8d2171e7f9b3b59bb7f1ce9
chidimi <daichi.imai@hotmail.com>
2021-05-17 01:28:59 +0900

    国際化対応

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope '(:locale)', locale: /en|nl/ do
+    resources :books
+  end
 end
