9a44f7d318dfb1b8300a27e2ee2213851dc04297
chidimi <daichi.imai@hotmail.com>
2021-05-23 00:02:10 +0900

    パスワード無しで更新可能に修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session_path
   end
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
   resources :books
   resources :users, only: %i[index show]
 end
