4475a926dadeae4f78502e40572ef4cf9d26bd8f
chidimi <daichi.imai@hotmail.com>
2021-06-19 19:09:33 +0900

    エラー時にrenderするよう変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
-  before_action :set_comment, only: %i[show edit update destroy]
-
   # POST /comments or /comments.json
   def create
     @comment = @commentable.comments.new(comment_params)
@@ -10,16 +8,11 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to @commentable, notice: 'Comment was successfully created.'
     else
-      redirect_to @commentable, flash: { error: @comment.errors.full_messages }
+      render @commentable_render
     end
   end
 
   private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_comment
-    @comment = Comment.find(params[:id])
-  end
 
   # Only allow a list of trusted parameters through.
   def comment_params
--- app/controllers/reports/comments_controller.rb
+++ app/controllers/reports/comments_controller.rb
@@ -2,10 +2,20 @@
 
 class Reports::CommentsController < CommentsController
   before_action :set_commentable
+  before_action :set_report
+  before_action :set_commentable_render
 
   private
 
   def set_commentable
     @commentable = Report.find(params[:report_id])
+  end
+
+  def set_report
+    @report = Report.find(params[:report_id])
+  end
+
+  def set_commentable_render
+    @commentable_render = 'reports/show'
   end
 end
